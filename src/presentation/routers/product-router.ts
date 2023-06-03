6fcb85cd3088461b1041808f2fe20435c9bae903
Veidz <joaovictorveidz@gmail.com>
2023-06-03 02:30:31 -0300

    feat: implement product router with post method

---
+++ src/presentation/routers/product-router.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import express, { type Request, type Response, type Router } from 'express'
+import { type CreateProductUseCase } from '../../domain/protocols/use-cases/product/create-product'
+
+export default function ProductsRouter(
+    createProductUseCase: CreateProductUseCase
+): Router {
+    const router = express.Router()
+
+    router.post('/', async(req: Request, res: Response) => {
+        try {
+            await createProductUseCase.execute(req.body)
+            res.status(201).json({ message: 'Created.' })
+        } catch (error) {
+            res.status(500).send({ message: 'Error saving data.' })
+        }
+    })
+
+    return router
+}
