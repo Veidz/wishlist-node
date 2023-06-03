1ce608f2d7da773c078991c2deb928dd60d606d1
Veidz <joaovictorveidz@gmail.com>
2023-06-03 19:06:02 -0300

    chore: add files to exclude from coverage and fixed folder to get coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,10 @@
 export default {
     roots: ['<rootDir>/src'],
-    collectCoverageFrom: ['<rootDir>/src/*.ts'],
+    collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
     coverageDirectory: 'coverage',
     testEnvironment: 'node',
     transform: {
         '.+\\.ts$': 'ts-jest'
-    }
+    },
+    coveragePathIgnorePatterns: ['<rootDir>/src/env.ts', '<rootDir>/src/main.ts']
 }
