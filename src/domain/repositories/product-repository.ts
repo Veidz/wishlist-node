38afde53887f82b5a450f242345ca8d00aa8ef0e
Veidz <joaovictorveidz@gmail.com>
2023-06-03 02:24:43 -0300

    feat: implement product repository

---
+++ src/domain/repositories/product-repository.ts
@@ -0,0 +1,15 @@
+import { type ProductRepository } from '../protocols/repositories/product-repository'
+import { type ProductRequestModel } from '../entities/product'
+import { type ProductDataSource } from '../../data/protocols/data-sources/product-data-source'
+
+export default class ProductRepositoryImpl implements ProductRepository {
+    productDataSource: ProductDataSource
+
+    constructor(productDataSource: ProductDataSource) {
+        this.productDataSource = productDataSource
+    }
+
+    async createProduct(product: ProductRequestModel): Promise<void> {
+        await this.productDataSource.create(product)
+    }
+}
