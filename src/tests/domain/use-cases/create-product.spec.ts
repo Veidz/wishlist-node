c4da9149827553ce38561d619e062101663912b5
Veidz <joaovictorveidz@gmail.com>
2023-06-03 19:14:29 -0300

    test: ensure CreateProductUseCase returns InsertOneResult with correct data

---
+++ src/tests/domain/use-cases/create-product.spec.ts
@@ -0,0 +1,42 @@
+import { ObjectId, type InsertOneResult } from 'mongodb'
+import { type ProductRequestModel } from '../../../domain/entities/product'
+import { type ProductRepository } from '../../../domain/protocols/repositories/product-repository'
+import CreateProduct from '../../../domain/use-cases/product/create-product'
+
+describe('Create Product Use Case', () => {
+    class MockProductRepository implements ProductRepository {
+        async createProduct(produt: ProductRequestModel): Promise<InsertOneResult> {
+            return {
+                acknowledged: true,
+                insertedId: new ObjectId('647bb3db14b88e8f2798d61e')
+            }
+        }
+    }
+
+    let mockProductRepository: ProductRepository
+
+    beforeEach(() => {
+        jest.clearAllMocks()
+        mockProductRepository = new MockProductRepository()
+    })
+
+    it('Should return InsertOneResult with correct data', async() => {
+        const inputData = {
+            name: 'any_name',
+            imageUrl: 'any_image_url',
+            productUrl: 'product_url'
+        }
+
+        jest.spyOn(mockProductRepository, 'createProduct').mockImplementation(async() => {
+            return {
+                acknowledged: true,
+                insertedId: new ObjectId('647bb3db14b88e8f2798d61e')
+            }
+        })
+        const createProductUseCase = new CreateProduct(mockProductRepository)
+        const result = await createProductUseCase.execute(inputData)
+        expect(result.acknowledged).toBe(true)
+        expect(result.insertedId).toEqual(new ObjectId('647bb3db14b88e8f2798d61e'))
+        expect(mockProductRepository.createProduct).toBeCalledTimes(1)
+    })
+})
