00501b58c9991a0e3fe32456703dbbeb854fcaa2
Veidz <joaovictorveidz@gmail.com>
2023-06-05 00:46:48 -0300

    chore: change mongo_url connection string

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 
 export default {
-    mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/wishlist',
+    mongoUrl: process.env.MONGO_URL ?? 'mongodb://root:root@localhost:27016',
     port: process.env.PORT ?? 5050
 }
